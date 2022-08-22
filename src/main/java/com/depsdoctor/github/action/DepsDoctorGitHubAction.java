17b2e6e63098e257d0cc51ab2aa3db943318b4e1
Felix <felix.carnicero@nauik.com>
2022-08-22 13:19:19 +0200

    New parameters

--- src/main/java/com/depsdoctor/github/action/DepsDoctorGitHubAction.java
+++ src/main/java/com/depsdoctor/github/action/DepsDoctorGitHubAction.java
@@ -3,6 +3,7 @@ package com.depsdoctor.github.action;
 import com.depsdoctor.github.action.config.ApiConfig;
 import com.depsdoctor.github.action.config.DepsDoctorConfig;
 import com.depsdoctor.github.action.http.HttpClientUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zeroturnaround.exec.ProcessExecutor;
@@ -17,6 +18,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static com.depsdoctor.github.action.Command.*;
 import static com.depsdoctor.github.action.utils.Files.unzipFile;
 import static com.depsdoctor.github.action.utils.Strings.isBlank;
 
@@ -38,35 +40,57 @@ public class DepsDoctorGitHubAction {
   private static final String DEFAULT_URL = "https://api.nauik.com/deps-doctor-service";
   private static final String USERNAME_PROPERTY = "username";
   private static final String PASSWORD_PROPERTY = "password";
-  private static final String COMMAND_PROPERTY = "command";
-  private static final String PROJECT_PROPERTY = "project";
-  private static final String DIRECTORY_PROPERTY = "directory";
+
+  private static final String WILOSECURL_PROPERTY = "wilosecurl";
 
   private static final String ZIP_FILE = "scanner.zip";
   private static final String DEPS_DOCTOR_DIR = "deps_doctor";
+
+  private static final String[] DEPS_DOCTOR_CMD = {"./deps-doctor", "scan", "-u"};
 
   public static void main(String[] args) throws Exception {
 
     log.info("Starting scanner action...");
 
+    String url = System.getProperty(WILOSECURL_PROPERTY, DEFAULT_URL);
     String username = System.getProperty(USERNAME_PROPERTY);
     String password = System.getProperty(PASSWORD_PROPERTY);
-    String command = System.getProperty(COMMAND_PROPERTY);
-    String project = System.getProperty(PROJECT_PROPERTY);
-    String dir = System.getProperty(DIRECTORY_PROPERTY);
+    Command command = Command.builder()
+        .project(System.getProperty(PROJECT_PROPERTY))
+        .directory(System.getProperty(DIRECTORY_PROPERTY))
+        .run(System.getProperty(RUN_PROPERTY))
+        .include(System.getProperty(INCLUDE_PROPERTY))
+        .exclude(System.getProperty(EXCLUDE_PROPERTY))
+        .output(System.getProperty(OUTPUT_PROPERTY))
+        .format(System.getProperty(FORMAT_PROPERTY))
+        .columns(System.getProperty(COLUMNS_PROPERTY))
+        .code(System.getProperty(CODE_PROPERTY))
+        .conf(System.getProperty(CONF_PROPERTY))
+        .baseline(System.getProperty(BASELINE_PROPERTY))
+        .customDetectorsDir(System.getProperty(CUSTOM_DETECTORS_DIR_PROPERTY))
+        .detectors(System.getProperty(DETECTORS_PROPERTY))
+        .skipDetectors(System.getProperty(SKIP_DETECTORS_PROPERTY))
+        .sbom(System.getProperty(SBOM_PROPERTY))
+        .sbomFormat(System.getProperty(SBOM_FORMAT_PROPERTY))
+        .secretsMode(System.getProperty(SECRETS_MODE_PROPERTY))
+        .standard(System.getProperty(STANDARD_PROPERTY))
+        .failOn(System.getProperty(FAIL_ON_PROPERTY))
+        .build();
 
-    if(isBlank(command) || isBlank(project) || isBlank(dir)) {
-      log.error("The parameters commands, project and directory are mandatory");
+    if(isBlank(command.getProject()) || isBlank(command.getDirectory())) {
+      log.error("The project and directory are mandatory");
+      if(!isBlank(command.getFailOn()) && command.getFailOn().equalsIgnoreCase("never")) {
+        System.exit(0);
+        return;
+      }
       System.exit(1);
       return;
     }
 
-    log.info(dir);
-
-    downloadScanner(DEFAULT_URL, username, password);
+    downloadScanner(url, username, password);
     unzipScanner();
     downloadCustomerConfig();
-    executeScanner(project, dir, command);
+    executeScanner(command);
 
     log.info("Scanner action  completed successfully.");
   }
@@ -85,10 +109,18 @@ public class DepsDoctorGitHubAction {
     file.setExecutable(true);
   }
 
-  private static void executeScanner(String project, String dir, String command) throws IOException, InterruptedException, TimeoutException {
+  private static void executeScanner(Command command) throws IOException, InterruptedException, TimeoutException {
+    //Exclude the scanner downloaded
+    if(isBlank(command.getExclude())) {
+      command.setExclude(DEPS_DOCTOR_DIR + "/deps-doctor/**");
+    }else{
+      command.setExclude(command.getExclude() + "," + DEPS_DOCTOR_DIR + "/deps-doctor/**");
+    }
+
+    String[] args = ArrayUtils.addAll(DEPS_DOCTOR_CMD, command.getCommandParams());
     new ProcessExecutor()
         .directory(new File(DEPS_DOCTOR_DIR + "/deps-doctor/"))
-        .command("./deps-doctor", command, "-n", project, "-d", dir, "-e", DEPS_DOCTOR_DIR + "/deps-doctor/**", "-u").timeout(60, TimeUnit.MINUTES)
+        .command(args).timeout(60, TimeUnit.MINUTES)
         .redirectError(Slf4jStream.of(log).asWarn())
         .redirectOutput(Slf4jStream.of(log).asInfo()).execute();
   }
